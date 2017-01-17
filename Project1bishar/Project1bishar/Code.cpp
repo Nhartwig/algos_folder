39a42b92bc5767dd86f53041dec2469874ab02cb
Anthony Bisulco <abisulco@gmail.com>
2017-01-17 16:29:59 -0500

    Algo Files

---
+++ Project1bishar/Project1bishar/Code.cpp
@@ -0,0 +1,21 @@
+//
+//  Code.cpp
+//  Project1bishar
+//
+//  Created by Anthony Bisulco on 1/17/17.
+//  Copyright © 2017 Anthony Bisulco. All rights reserved.
+//
+
+#include "Code.hpp"
+/*class Code{
+private:
+    std::vector<int> seq;
+    void seqInitialize(int n = 0, int m = 0);
+public:
+    Code(int n = 0,int m = 0);
+    int checkCorrect(Code guess);
+    int checkIncorrect(Code guess);
+    std::vector<int> getCode();
+};*/
+
+
---
+++ Project1bishar/Project1bishar/Code.hpp
@@ -0,0 +1,30 @@
+//
+//  Code.hpp
+//  Project1bishar
+//
+//  Created by Anthony Bisulco on 1/17/17.
+//  Copyright © 2017 Anthony Bisulco. All rights reserved.
+//
+
+#ifndef Code_hpp
+#define Code_hpp
+
+#include <stdio.h>
+#include<vector>
+
+class Code{
+    private:
+        std::vector<int> seq;
+        void seqInitialize(int n = 0, int m = 0);
+    public:
+        Code(int n = 0,int m = 0);
+        int checkCorrect(Code guess);
+        int checkIncorrect(Code guess);
+        std::vector<int> getCode();
+};
+
+
+
+
+
+#endif /* Code_hpp */
